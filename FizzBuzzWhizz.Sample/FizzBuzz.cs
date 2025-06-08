7f0049f39b9d5ea07599ac213a070f74cb57a731
Jodre11 <jodre11.public@gmail.com>
2025-06-08 11:27:02 +0100

    Add Roslyn source generator implementation and tests
    Implemented a Roslyn-based source generator with sample projects demonstrating its usage (FizzBuzzWhizz). Added unit tests validating the generated code and updated the solution structure, including updated configurations in `.gitignore`, `.gitattributes`, and `.editorconfig`. Detailed examples and instructions are documented in the `README.md`.

---
+++ FizzBuzzWhizz.Sample/FizzBuzz.cs
@@ -0,0 +1,10 @@
+using FizzBuzzWhizz;
+
+namespace FizzBuzzWhizz.Sample;
+
+// This code will not compile until you build the project with the Source Generators
+
+[FizzBuzzWhizz("Fizz", "3", "Buzz", "5")]
+public partial class FizzBuzz
+{
+}
