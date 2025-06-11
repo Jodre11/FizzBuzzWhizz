2a0724ca361c2e01f34ff9fba0d1decf67a135ae
Jodre11 <jodre11.public@gmail.com>
2025-06-11 20:18:51 +0100

    Add FizzBuzz implementations and performance comparison benchmarks

---
+++ FizzBuzzWhizz.Sample/FizzBuzzWhizz.cs
@@ -0,0 +1,8 @@
+﻿using FizzBuzzWhizz;
+
+namespace FizzBuzzWhizz.Sample;
+
+// This code will not compile until you build the project with the Source Generators
+
+[FizzBuzzWhizz("Fizz", "3", "Buzz", "5", "Whizz", "7")]
+public partial class FizzBuzzWhizz;
---
+++ FizzBuzzWhizz.Sample/IdentityPerformanceComparison.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Jobs;
+
+namespace FizzBuzzWhizz.Sample;
+
+[SimpleJob(RuntimeMoniker.Net90)]
+[SimpleJob(RuntimeMoniker.NativeAot90)]
+[RPlotExporter]
+public class IdentityPerformanceComparison
+{
+    private static readonly FizzBuzzManualManyModuloOneByOne FizzBuzzManualManyModuloOneByOne = new();
+    private static readonly FizzBuzzManualManyModuloUpFront FizzBuzzManualManyModuloUpFront = new();
+    private static readonly FizzBuzzManualSingleModuloManyPatterns FizzBuzzManualSingleModuloManyPatterns = new();
+
+    private static readonly Consumer Consumer = new();
+
+    private static void Tester(Func<long, string> identityFunction)
+    {
+        for (long i = 0; i <= 60; i++)
+        {
+            Consumer.Consume(identityFunction(i));
+        }
+    }
+
+    [Benchmark]
+    public void FizzBuzzManualManyModuloOneByOneTest() =>
+        Tester(FizzBuzzManualManyModuloOneByOne.Identity);
+
+    [Benchmark]
+    public void FizzBuzzManualManyModuloUpFrontTest() =>
+        Tester(FizzBuzzManualManyModuloUpFront.Identity);
+
+    [Benchmark]
+    public void FizzBuzzManualSingleModuloManyPatternsTest() =>
+        Tester(FizzBuzzManualSingleModuloManyPatterns.Identity);
+}
